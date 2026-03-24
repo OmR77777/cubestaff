28298e8c58a9105dfba804c767ae0cffeb7ba999
OmR77777 <benamirayazid49@gmail.com>
2026-03-24 22:14:23 +0100

    Update CubeSTAFF.java

--- CubeSTAFF.java
+++ CubeSTAFF.java
@@ -1,10 +1,5 @@
 package omr.cubeSTAFF;
 
-// ╔══════════════════════════════════════════════════════════════════╗
-// ║                   CubeSTAFF v4.0                                ║
-// ║  ملف واحد - نظام إدارة فريق الإدارة المتكامل                  ║
-// ╚══════════════════════════════════════════════════════════════════╝
-
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.EmbedBuilder;
